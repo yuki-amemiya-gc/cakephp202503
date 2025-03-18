a534252d278351499b637d9b36c2033446dcbdc1
YoshinobuSakuraba <152175225+YoshinobuSakuraba@users.noreply.github.com>
2025-03-18 20:52:31 +0900

    ユーザテーブル作成しました（bakeコマンド使用）

---
+++ web/app/src/Model/Entity/User.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * User Entity
+ *
+ * @property int $id
+ * @property string $account
+ * @property string $password
+ * @property string $name
+ * @property string $email
+ * @property \Cake\I18n\FrozenTime $created
+ * @property \Cake\I18n\FrozenTime $modified
+ * @property string|null $created_user
+ * @property string|null $modified_user
+ */
+class User extends Entity
+{
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array<string, bool>
+     */
+    protected $_accessible = [
+        'account' => true,
+        'password' => true,
+        'name' => true,
+        'email' => true,
+        'created' => true,
+        'modified' => true,
+        'created_user' => true,
+        'modified_user' => true,
+    ];
+
+    /**
+     * Fields that are excluded from JSON versions of the entity.
+     *
+     * @var array<string>
+     */
+    protected $_hidden = [
+        'password',
+    ];
+}
---
+++ web/app/tests/Fixture/UsersFixture.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'account' => 'Lorem ipsum dolor ',
+                'password' => 'Lorem ipsum dolor sit amet',
+                'name' => 'Lorem ipsum dolor ',
+                'email' => 'Lorem ipsum dolor sit amet',
+                'created' => 1742214957,
+                'modified' => 1742214957,
+                'created_user' => 'Lorem ipsum dolor sit amet',
+                'modified_user' => 'Lorem ipsum dolor sit amet',
+            ],
+        ];
+        parent::init();
+    }
+}
