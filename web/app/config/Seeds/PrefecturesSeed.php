8d5bd9f74d0c34b27c1b29300b37e8400ac99be9
YoshinobuSakuraba <152175225+YoshinobuSakuraba@users.noreply.github.com>
2025-03-20 15:46:49 +0900

    PrefecturesSeedファイルの実行

--- web/app/config/Seeds/PrefecturesSeed.php
+++ web/app/config/Seeds/PrefecturesSeed.php
@@ -16,7 +16,7 @@ class PrefecturesSeed extends AbstractSeed
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
         $data = [
             [
