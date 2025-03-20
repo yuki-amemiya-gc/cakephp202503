b093f48399562d5e7b409e479d7f9c026a5d751c
YoshinobuSakuraba <152175225+YoshinobuSakuraba@users.noreply.github.com>
2025-03-20 13:11:25 +0900

    cakephp202503/web/app/src/Model/Entity/User.phpへパスワードハッシュ化メソッドの追加

--- web/app/src/Model/Entity/User.php
+++ web/app/src/Model/Entity/User.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Authentication\PasswordHasher\DefaultPasswordHasher;
 
 /**
  * User Entity
@@ -53,4 +54,11 @@ class User extends Entity
     protected $_hidden = [
         'password',
     ];
+
+    protected function _setPassword(string $password) : ?string
+    {
+        if (strlen($password) > 0) {
+        return (new DefaultPasswordHasher())->hash($password);
+      }
+    }    
 }
