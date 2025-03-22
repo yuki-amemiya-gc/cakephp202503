1119e6c733630009f2853325a1c0cbe20ace38e3
YoshinobuSakuraba <152175225+YoshinobuSakuraba@users.noreply.github.com>
2025-03-22 14:10:27 +0900

    postsテーブルを作成し、差分マイグレーションファイルを生成

---
+++ web/app/config/Migrations/20250322050916_CreatTablePosts.php
@@ -0,0 +1,83 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class CreatTablePosts extends AbstractMigration
+{
+    /**
+     * Up Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-up-method
+     * @return void
+     */
+    public function up(): void
+    {
+        $this->table('posts')
+            ->addColumn('user_id', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('content', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addIndex(
+                [
+                    'user_id',
+                ],
+                [
+                    'name' => 'posts_ibfk_1',
+                ]
+            )
+            ->create();
+
+        $this->table('posts')
+            ->addForeignKey(
+                'user_id',
+                'users',
+                'id',
+                [
+                    'update' => 'NO_ACTION',
+                    'delete' => 'CASCADE',
+                    'constraint' => 'posts_ibfk_1'
+                ]
+            )
+            ->update();
+    }
+
+    /**
+     * Down Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-down-method
+     * @return void
+     */
+    public function down(): void
+    {
+        $this->table('posts')
+            ->dropForeignKey(
+                'user_id'
+            )->save();
+
+        $this->table('posts')->drop()->save();
+    }
+}
